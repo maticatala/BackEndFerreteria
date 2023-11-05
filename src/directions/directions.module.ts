fd62a366606cafbaf9050bff33c48ec2b3421c28
Faustino <faustinognavi@gmail.com>
2023-11-05 12:47:32 -0300

    solucion de errores

--- src/directions/directions.module.ts
+++ src/directions/directions.module.ts
@@ -5,14 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/auth/entities/user.entity';
 import { Pedido } from 'src/pedidos/entities/pedido.entity';
-
+import { Direction } from './entities/direction.entity';
+import { PedidosService } from 'src/pedidos/pedidos.service';
+import { Product } from 'src/products/entities/product.entity';
 @Module({
   imports:[
     ConfigModule.forRoot(),
     TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([Direction]),
     TypeOrmModule.forFeature([Pedido]),
+    TypeOrmModule.forFeature([Product]),
   ],
   controllers: [DirectionsController],
-  providers: [DirectionsService],
+  providers: [DirectionsService,PedidosService]
 })
 export class DirectionsModule {}
--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -5,15 +5,20 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Direction } from 'src/directions/entities/direction.entity';
 import { User } from 'src/auth/entities/user.entity';
+import { Pedido } from './entities/pedido.entity';
+import { DirectionsService } from 'src/directions/directions.service';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
   imports:[
     ConfigModule.forRoot(),
     TypeOrmModule.forFeature([User]),
     TypeOrmModule.forFeature([Direction]),
+    TypeOrmModule.forFeature([Pedido]),
+    TypeOrmModule.forFeature([Product]),
     
   ],
   controllers: [PedidosController],
-  providers: [PedidosService],
+  providers: [PedidosService,DirectionsService],
 })
 export class PedidosModule {}
