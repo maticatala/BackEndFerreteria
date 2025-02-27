be2d58df56a52716a2ae613d7f0c70f9bb28442d
Tiago Pieroni <tepieroni@gmail.com>
2025-02-27 20:23:51 -0300

    contact envio de mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,37 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { dataSourceOptions } from 'db/data-source';
 import { CurrentUserMiddleware } from './utility/middlewares/current-user.middleware';
+import { ContactModule } from './contact/contact.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAIL_HOST'),
+          port: configService.get('MAIL_PORT'),
+          secure: configService.get('MAIL_SECURE') === 'true',
+          auth: {
+            user: configService.get('MAIL_USER'),
+            pass: configService.get('MAIL_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: configService.get('MAIL_FROM'),
+        },
+      }),
+    }),
     AuthModule,
     CategoriesModule,
     ProductsModule,
     OrdersModule,
+    ContactModule,
+    
   ],
   controllers: [],
   providers: [],
