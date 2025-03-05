cf0d4f4765c08302b18932a38135936b946a0f70
Faustino <faustinognavi@gmail.com>
2025-03-04 22:08:55 -0300

    ultimos cambios de mails

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -165,6 +165,8 @@ export class AuthService {
   ): Promise<void> {
     const { email } = requestResetPasswordDto;
 
+
+    console.log(email);
     const user = await this.validateUserExistsByEmail(email);
 
     await this.assignResetPasswordToken(user);
